fbf45cd13c348a54e694e3f345078cb06562b1eb
Jordan Vincent <thelifeofjtv@gmail.com>
2022-10-21 19:27:54 -0400

    Test Commit

--- BeSpoked/Startup.cs
+++ BeSpoked/Startup.cs
@@ -17,7 +17,7 @@ namespace BeSpoked
         {
             Configuration = configuration;
         }
-
+        // Test commit before changes
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
